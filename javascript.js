7ce9c2deada7295651c69ed57059e5c993975ed6
Mason-Harmon <144073259+Mason-Harmon@users.noreply.github.com>
2023-12-15 23:48:22 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function changeColor(squareid){
+    var square = document.getElementById(squareid);
+    var randomColor = getRandomColor();
+    square.style.background = randomColor;
+    alert("Color Changed");
+}
+
+alert("this is working!")
+function getRandomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function flashColors(){
+    var flashes = document.querySelectorAll('.flash');
+    flashes.forEach(changeflash());
+    function changeflash(){
+        for ( var i=0; i < 9; i++){
+            var rcolor = getRandomColor()
+            var ark = flashes[i]
+            ark.style.backgroundColor = rcolor;
+        }
+    }
+
+}
+setInterval(flashColors, 100)
